e5bea3aa4436100ab94b436b317be7ffea9c6cbc
MarcosOliveir4 <marcosassis071@gmail.com>
2021-12-18 09:26:03 -0300

    :art: add style global and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export const App: React.FC = () => {
-  return <h1>Template inicial</h1>;
+  return (
+    <>
+      <h1>Template inicial</h1>
+      <GlobalStyle />
+    </>
+  );
 };
